dd66e8d51e49b94828010fd63ac91d618af933b4
Centeno448 <diego.centeno@udla.edu.ec>
2021-06-01 18:52:03 -0500

    added missing test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import TareasLista from './TareasLista';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [ tareas, setTareas ] = useState ([ ])
+  const [tareas, setTareas] = useState([]);
 
   const tareaNombreRef = useRef();
 
-  function agregarTarea(e){
+  function agregarTarea(e) {
     const nombre = tareaNombreRef.current.value;
 
-    if(nombre ===''){
+    if (nombre === '') {
       return;
     }
 
-    setTareas(prevTareas => {
-      return [...prevTareas, {id: uuidv4(), nombre: nombre, completado: false}]
-    })
+    setTareas((prevTareas) => {
+      return [
+        ...prevTareas,
+        { id: uuidv4(), nombre: nombre, completado: false },
+      ];
+    });
 
     tareaNombreRef.current.value = null;
   }
 
   return (
     <>
-    <TareasLista tareas={tareas}/>
-    <input ref={tareaNombreRef} type="text" />
-    <button onClick={agregarTarea}>Añadir Tarea</button>
-    <button>Limpiar Tareas Completadas</button>
-    <div>0 tareas por terminar</div>
+      <TareasLista tareas={tareas} />
+      <input ref={tareaNombreRef} type="text" />
+      <button onClick={agregarTarea}>Añadir Tarea</button>
+      <button>Limpiar Tareas Completadas</button>
+      <div>0 tareas por terminar</div>
     </>
-  )
+  );
 }
 
 export default App;
